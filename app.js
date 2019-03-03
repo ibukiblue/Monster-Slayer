61d95cd533952c803209a828dd1615164c84d14a
ibukiblue <blueandchacha@gmail.com>
2019-03-03 17:25:11 +0900

    add-attack

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRuning: false,
-    
+    gameIsRunning: true,
+    turns: []
+  },
+  methods: {
+    startGame:() => {
+      this.gameIsRunning = true,
+      this.playerHealth = 100,
+      this.monsterHealth = 100,
+      this.turns = []
+    },
+    attack: () => {
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      })
+    },
+    calculateDamage: (min, max) => {
+      return Math.max(Math.floor(Math.random() * max + 1), min);
+    }
   }
 })
